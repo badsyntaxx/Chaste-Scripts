0760796f710f64e80048c882dd1c3363f1e5427a
badsyntaxx <badsyntaxx@gmail.com>
2024-03-24 12:21:33 -0600

    User name editor updated

--- Scripts/Edit-UserName.ps1
+++ Scripts/Edit-UserName.ps1
@@ -1,118 +1,48 @@
-
-function Invoke-This {
-    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
-        Exit
-    }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
+    Exit
+}
     
-    $scriptName = "Edit-UserName"
-    $scriptPath = $env:TEMP
 
-    if (Get-Content -Path "$PSScriptRoot\CS-Framework.ps1" -ErrorAction SilentlyContinue) {
-        $framework = Get-Content -Path "$PSScriptRoot\CS-Framework.ps1" -Raw
-    } else {
-        Get-Script -Url "https://raw.githubusercontent.com/badsyntaxx/Chaste-Scripts/main/CS-Framework.ps1" -Target "$scriptPath\CS-Framework.ps1"
-        $framework = Get-Content -Path "$scriptPath\CS-Framework.ps1" -Raw
-        Get-Item -ErrorAction SilentlyContinue "$scriptPath\CS-Framework.ps1" | Remove-Item -ErrorAction SilentlyContinue
-    }
-
-    $scriptDescription = @"
+function Edit-UserName {
+    try {
+        $scriptDescription = @"
  This script allows you to modify the username of a user on a Windows system. 
 "@
 
-    $core = @"
-function $scriptName {
-    try {
-        Get-Item -ErrorAction SilentlyContinue "$scriptPath\$scriptName.ps1" | Remove-Item -ErrorAction SilentlyContinue
+        Get-Item -ErrorAction SilentlyContinue "$scriptPath\Edit-UserName.ps1" | Remove-Item -ErrorAction SilentlyContinue
         Write-Host " Chaste Scripts: Edit User Name v0315242300"
         Write-Host "$scriptDescription" -ForegroundColor DarkGray
 
-        `$username = Select-User
+        $username = Select-User
 
         Write-Text -Type "header" -Text "Enter username" -LineBefore -LineAfter
-        `$newName = Get-Input -Prompt "" -Validate "^(\s*|[a-zA-Z0-9 _\-]{1,64})$" -CheckExistingUser
+        $newName = Get-Input -Prompt "" -Validate "^(\s*|[a-zA-Z0-9 _\-]{1,64})$" -CheckExistingUser
 
         Write-Text -Type "notice" -Text "You're about to change this users name." -LineBefore -LineAfter
-        `$choice = Get-Option -Options `$([ordered]@{
-            "Submit"  = "Confirm and apply." 
-            "Reset"   = "Start over at the beginning."
-            "Exit"    = "Run a different command."
-        }) -LineAfter
+        $choice = Get-Option -Options $([ordered]@{
+                "Submit" = "Confirm and apply." 
+                "Reset"  = "Start over at the beginning."
+                "Exit"   = "Run a different command."
+            }) -LineAfter
 
-        if (`$choice -ne 0 -and `$choice -ne 1 -and `$choice -ne 2) { $scriptName }
-        if (`$choice -eq 1) { Invoke-Script "$scriptName" }
-        if (`$choice -eq 2) { Write-Exit -Script "$scriptName" }
+        if ($choice -ne 0 -and $choice -ne 1 -and $choice -ne 2) { Edit-UserName }
+        if ($choice -eq 1) { Invoke-Script "Edit-UserName" }
+        if ($choice -eq 2) { Write-Exit -Script "Edit-UserName" }
     
-        Rename-LocalUser -Name `$username -NewName `$newName
+        Rename-LocalUser -Name $username -NewName $newName
 
-        `$newUser = Get-LocalUser -Name `$newName
+        $newUser = Get-LocalUser -Name $newName
 
-        if (`$null -ne `$newUser) { 
-            `$data = Get-UserData `$newName
-            Write-Text -Type "list" -List `$data -LineAfter
-            Write-Exit "The name for this account has been changed." -Script "$scriptName"
+        if ($null -ne $newUser) { 
+            $data = Get-UserData $newName
+            Write-Text -Type "list" -List $data -LineAfter
+            Write-Exit "The name for this account has been changed." -Script "Edit-UserName"
         } else {
             Write-Text -Type "fail" -Text "There was an unknown error when trying to rename this user."
         }
     } catch {
-        Write-Text -Type "error" -Text "Edit name error: `$(`$_.Exception.Message)"
-        Write-Exit -Script "$scriptName"
+        Write-Text -Type "error" -Text "Edit name error: $($_.Exception.Message)"
+        Write-Exit -Script "Edit-UserName"
     }
 }
-
-"@
-
-    New-Item -Path "$scriptPath\$scriptName.ps1" -ItemType File -Force | Out-Null
-
-    Add-Content -Path "$scriptPath\$scriptName.ps1" -Value $core
-    Add-Content -Path "$scriptPath\$scriptName.ps1" -Value $framework
-    Add-Content -Path "$scriptPath\$scriptName.ps1" -Value "Invoke-Script '$scriptName'"
-
-    Start-Process powershell.exe "-NoProfile -NoExit -ExecutionPolicy Bypass -File `"$scriptPath\$scriptName.ps1`"" -WorkingDirectory $pwd -Verb RunAs
-}
-
-function Get-Script {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Url,
-        [Parameter(Mandatory)]
-        [string]$Target
-    )
-    
-    try {
-        $request = [System.Net.HttpWebRequest]::Create($Url)
-        $response = $request.GetResponse()
-  
-        if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) {
-            throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$Url'."
-        }
-
-        [byte[]]$buffer = new-object byte[] 1048576
-        [long]$total = [long]$count = 0
-  
-        $reader = $response.GetResponseStream()
-        $writer = new-object System.IO.FileStream $Target, "Create"
-  
-        do {
-            $count = $reader.Read($buffer, 0, $buffer.Length)
-            $writer.Write($buffer, 0, $count)
-            $total += $count
-        } while ($count -gt 0)
-        if ($count -eq 0) { return $true } else { return $false }
-    } catch {
-        Write-Host "Loading failed..."
-            
-        if ($retryCount -lt $MaxRetries) {
-            Write-Host "Retrying..."
-            Start-Sleep -Seconds $Interval
-        } else {
-            Write-Host "Maximum retries reached. Loading failed."
-        }
-    } finally {
-        if ($reader) { $reader.Close() }
-        if ($writer) { $writer.Flush(); $writer.Close() }
-        [GC]::Collect()
-    } 
-}   
-
-Invoke-This
