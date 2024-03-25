6c56c6bff10bcccaff3b9b75ffdf2f11191d15b6
badsyntaxx <badsyntaxx@gmail.com>
2024-03-25 14:44:02 -0600

    Installer now deletes when script is done

--- InTech/Install-IsrNinja.ps1
+++ InTech/Install-IsrNinja.ps1
@@ -5,26 +5,24 @@ function Install-IsrNinja {
         Write-Text -Type "header" -Text "Installing NinjaOne for Nuvia ISR Center" -LineBefore -LineAfter
 
         $url = "https://app.ninjarmm.com/agent/installer/0274c0c3-3ec8-44fc-93cb-79e96f191e07/nuviaisrcenteroremut-5.7.8836-windows-installer.msi"
-        $appName = "NinjaOne"
         $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
 
         if ($null -ne $service -and $service.Status -eq "Running") {
-            $download = Get-Download -Url $Url -Target "$env:TEMP\$AppName.msi"
+            $download = Get-Download -Url $Url -Target "$env:TEMP\NinjaOne.msi"
             if (!$download) { throw "Unable to acquire intaller." }
           
-            Start-Process -FilePath "msiexec" -ArgumentList "/i `"$env:TEMP\$AppName.msi`" /qn" -Wait
+            Start-Process -FilePath "msiexec" -ArgumentList "/i `"$env:TEMP\NinjaOne.msi`" /qn" -Wait
 
             $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
-
             if ($null -ne $service -and $service.Status -eq "Running") {
-                Write-Text -Type "success" -Text "$AppName successfully installed." -LineAfter
+                Write-Text -Type "success" -Text "NinjaOne successfully installed." -LineAfter
             } else {
-                Write-Text -Type "error" -Text "$AppName did not successfully install." -LineAfter
+                Write-Text -Type "error" -Text "NinjaOne did not successfully install." -LineAfter
             }
         } else {
             Write-Text -Type "done" -Text "$service is already installed and running."
         }
-
+        Get-Item -ErrorAction SilentlyContinue "$env:TEMP\NinjaOne.msi" | Remove-Item -ErrorAction SilentlyContinue
         Write-Exit -Script "Install-IsrNinja"
     } catch {
         Write-Text -Type "error" -Text "Install error: $($_.Exception.Message)"
